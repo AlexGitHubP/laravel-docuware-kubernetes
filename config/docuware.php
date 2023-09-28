41009ce21ce743f739e4d71180417afeb0e8b41b
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2023-09-28 11:59:18 +0300

    Add files via upload

--- config/docuware.php
+++ config/docuware.php
@@ -1,1 +1,48 @@
+<?php
 
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | DocuWare Credentials
+    |--------------------------------------------------------------------------
+    |
+    | Before you can communicate with the DocuWare REST-API it is necessary
+    | to enter your credentials. You should specify a url containing the
+    | scheme and hostname. In addition add your username and password.
+    |
+    */
+
+    'credentials' => [
+        'url' => env('DOCUWARE_URL'),
+        'username' => env('DOCUWARE_USERNAME'),
+        'password' => env('DOCUWARE_PASSWORD'),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Passphrase
+    |--------------------------------------------------------------------------
+    |
+    | In order to create encrypted URLs we need a passphrase. This enables a
+    | secure exchange of DocuWare URLs without anyone being able to modify
+    | your query strings. You can find it in the organization settings.
+    |
+    */
+
+    'passphrase' => env('DOCUWARE_PASSPHRASE'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Cookie Lifetime
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define the amount of minutes the authentication cookie is
+    | valid. Afterwards it will be removed from the cache and you need to
+    | provide a fresh one. By default, the lifetime lasts for one year.
+    |
+    */
+
+    'cookie_lifetime' => (int) env('DOCUWARE_COOKIE_LIFETIME', 525600),
+
+];
